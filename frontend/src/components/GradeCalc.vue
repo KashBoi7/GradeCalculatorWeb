23c1d880587881a9b885123dfb0eebaadcbd5909
KashBoi7 <77999576+KashBoi7@users.noreply.github.com>
2022-07-07 12:52:22 -0700

    d

--- frontend/src/components/GradeCalc.vue
+++ frontend/src/components/GradeCalc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="hero">
       <!-- <h1
         class="text-center bg-primary text-white"
         style="border-radius: 10px"
@@ -7,34 +7,49 @@
         Grade Calculator
       </h1> -->
     <div class="mid-box">
-      <div class="row">
-        <div class="col-sm-12">
-          <table class="table table-hover" style="width:100%">
+      <div>
+        <div>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/minty/bootstrap.min.css" integrity="sha384-H4X+4tKc7b8s4GoMrylmy2ssQYpDHoqzPa9aKXbDwPoPUA3Ra8PA5dGzijN+ePnH" crossorigin="anonymous">
+          <table class="table no" style="width:100%">
           <thead>
               <tr >
                 <th scope="col" class="white">Category</th>
-                <th scope="col" class="white">Grade(%)</th>
-                <th scope="col" class="white">Grade Letter</th>
-                <th scope="col" required class="white">Weight</th>
+                <th scope="col">Grade(%)</th>
+                <th class="white" scope="col" >Grade Letter</th>
+                <th scope="col" >
+                  <span class="white">
+                    Weight(%)
+                  </span>
+                </th>
               </tr>
            </thead>
            <tbody>
-              <tr v-for="index in 4" :key="index">
+              <tr v-for="index in x" :key="index">
                 <td scope="col">
-                  <input class="box">
+                  <input v-if="index == 1" class="box" placeholder="e.g. Participation">
+                  <input v-else-if="index == 2" class="box" placeholder="e.g. Homework">
+                  <input v-else class="box">
                 </td>
                 <td scope="col">
-                  <input class="small-box box">
+                  <input v-if="index == 1" class="small-box box" placeholder="e.g. 91">
+                  <input v-else-if="index == 2" class="small-box box" placeholder="e.g. 94">
+                  <input v-else class="small-box box">
                 </td>
                 <td scope="col">
-                  <input class="small-box box">
+                  <input v-if="index == 1" class="small-box box" placeholder="e.g. A-">
+                  <input v-else-if="index == 2" class="small-box box" placeholder="e.g. A">
+                  <input v-else class="small-box box">
                 </td>
                 <td scope="col" >
-                  <input class="small-box box">
+                  <input v-if="index == 1" class="small-box box" placeholder="e.g. 10">
+                  <input v-else-if="index == 2" class="small-box box" placeholder="e.g. 20">
+                  <input v-else class="small-box box">
                 </td>
               </tr>
            </tbody>
           </table>
+          <button @click="increase" type="button" class="btn btn-outline-dark">Add Class</button>
+          <button type="button" class="btn btn-outline-success">Calculate</button>
         </div>
       </div>
     </div>
@@ -45,7 +60,18 @@
 // @ is an alias to /src
 export default {
   name: 'GradeCalc',
-  components: {}
+  components: {},
+  data () {
+    return {
+      x: 4
+    }
+  },
+  methods: {
+    increase () {
+      this.x++
+    }
+  }
+
 }
 </script>
 
@@ -54,7 +80,7 @@ export default {
     width: 570px;
     height: 75%;
     border-radius: 10px;
-    background-color: rgba(220, 222, 220);
+    background-color: rgba(250, 250, 250,0.8);
     position: absolute;
     top:0;
     bottom: 0;
@@ -63,24 +89,36 @@ export default {
     margin: auto;
     align-items: center;
     box-shadow: 5px 5px 5px rgba(0,0,0,0.2);
-    .white{
-      foreground-color: white;
+    tr{
+      outline: 0;
     }
+    .table {
+      border: none;
+    }
+    .btn{
+      margin-right: 10px;
+    }
+}
+.white {
+  font-color: white;
 }
 .box{
   display: block;
   padding: 10px 6px;
-  background-color : rgba(220, 222, 220,0.8);
+  background-color : transparent;
+  color: black;
   box-sizing: border-box;
   border: none;
-  border-bottom: 2px solid rgb(255, 255, 255);
+  border-bottom: 2px solid black;
   height: 35px;
+  outline: 0;
+  font-size: 16px;
 }
 .small-box{
   width:90px;
 }
 .hero {
-  background-image: url("../assets/hero-bg.png");
+  background-image: url("..\\assets\\5291450.jpg");
   background-attachment: fixed;
   position: relative;
   height: 100vh;
